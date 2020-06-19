5cdec9cf5080fd7e11c0d348d4c90aa739d77e40
krishnakammari <63136494+krishnakammari@users.noreply.github.com>
2020-06-19 12:24:52 +0530

    Updates

---
+++ precache-manifest.d92c3ba399c618ab5cecbfd4169cc23e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1e1fcec0801cce33330e79e357cb165",
+    "url": "/hostel-finder/index.html"
+  },
+  {
+    "revision": "e7b9b6e409dae422ca1b",
+    "url": "/hostel-finder/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "e7ed948a412cdc8a709f",
+    "url": "/hostel-finder/static/css/main.e26dde28.chunk.css"
+  },
+  {
+    "revision": "e7b9b6e409dae422ca1b",
+    "url": "/hostel-finder/static/js/2.1ba7dfe4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hostel-finder/static/js/2.1ba7dfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7ed948a412cdc8a709f",
+    "url": "/hostel-finder/static/js/main.36a6d6d4.chunk.js"
+  },
+  {
+    "revision": "71879c86e15898cbb8b8",
+    "url": "/hostel-finder/static/js/runtime-main.68d021fb.js"
+  }
+]);
